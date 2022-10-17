5ff6ce9a4cc323faf3698ab455e16bb6d0b1fa18
Deepak <54444824+deepak852k1@users.noreply.github.com>
2022-10-17 11:53:40 +0530

    Create Basic.java

---
+++ Recursion/Basic.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	message();
+    }
+    static void message()
+    {
+        System.out.println("Hello WOrld");
+        message1();
+    }
+    static void message1()
+    {
+        System.out.println("Hello WOrld");
+        message2();
+    }static void message2()
+    {
+        System.out.println("Hello WOrld");
+        message3();
+    }static void message3()
+    {
+        System.out.println("Hello WOrld");
+        message4();
+    }static void message4()
+    {
+        System.out.println("Hello World");
+    }
+
+
+
+}
